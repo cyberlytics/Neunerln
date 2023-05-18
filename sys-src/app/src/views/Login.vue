99a015e886928a1322e51a59d3e1f40c9e641674
Franziska Rubenbauer <f.rubenbauer@oth-aw.de>
2023-05-18 16:36:15 +0200

    First Layout
    Former-commit-id: cc0806f7a55ca7a6de5563b63fa1531aa116fa7c

---
+++ sys-src/app/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <head> </head>
+
+  <body>
+    <!-- TODO: 1 -Poblem: Not good aligned-->
+    <form id="LoginForm">
+      <div class="heading">Login</div>
+      <label>
+        Benutzername
+        <input class="inputField" type="text" placeholder="NeunerlnGeed2000" required />
+      </label>
+      <br />
+      <label>
+        Password
+        <input class="inputField" type="password" placeholder="Over9000" required />
+      </label>
+    </form>
+    <button class="button" type="submit" form="LoginForm">Login</button>
+  </body>
+</template>
