d8a498b27759cfc0032462083d02b5394485ebd9
Kevin Paulus <k.paulus@oth-aw.de>
2023-05-22 12:48:08 +0200

    applied prettier config
    previous commits didn't use prettier in webstorm
    
    
    Former-commit-id: 262557e5e671765ad73047ceffbb6035ef0e8d12

--- sys-src/server/src/model/User.ts
+++ sys-src/server/src/model/User.ts
@@ -1,40 +1,40 @@
-import mongoose from "mongoose";
-const { Schema, model } = mongoose;
+import mongoose from 'mongoose'
+const { Schema, model } = mongoose
 
 // Document interface
-interface IUser{
-  name: string;
-  email: string;
-  password: string;
-  played?: number;
-  won?: number;
+interface IUser {
+  name: string
+  email: string
+  password: string
+  played?: number
+  won?: number
 }
 
 // Schema
 const userSchema = new Schema<IUser>({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   played: {
     type: Number,
-    default: 0,
+    default: 0
   },
   won: {
     type: Number,
-    default: 0,
+    default: 0
   }
-});
+})
 
 // Model
-const User = model<IUser>("User", userSchema);
-export default User;
+const User = model<IUser>('User', userSchema)
+export default User
--- sys-src/server/src/server.ts
+++ sys-src/server/src/server.ts
@@ -5,8 +5,8 @@ const start = async () => {
   try {
     // connect to database here
     await mongoose.connect(
-      "mongodb+srv://Admin:ebewnO8vJqdoKnVp@neunerln.hipwzzh.mongodb.net/?retryWrites=true&w=majority"
-    );
+      'mongodb+srv://Admin:ebewnO8vJqdoKnVp@neunerln.hipwzzh.mongodb.net/?retryWrites=true&w=majority'
+    )
   } catch (err) {
     console.error(err)
   }
