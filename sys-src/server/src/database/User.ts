be277777dacddd589bc6dd30a34ce1cd67e5dfa3
Kevin Paulus <k.paulus@oth-aw.de>
2023-06-11 18:48:58 +0200

    fix database functions
    Former-commit-id: a18f7d9b7eba3915858460879494c7811667c1e7

--- sys-src/server/src/database/User.ts
+++ sys-src/server/src/database/User.ts
@@ -5,9 +5,9 @@ import User from '../model/User'
 export async function createUser(name: string, email: string, password: string) {
   try {
     return User.create({
-      name: name,
-      email: email,
-      password: password
+      name,
+      email,
+      password
     })
   } catch (err: any) {
     throw new DatabaseError(err)
@@ -17,15 +17,31 @@ export async function createUser(name: string, email: string, password: string) 
 //Find data
 //by email
 export async function findUserByEmail(email: string) {
-  return User.findOne({ email: email }, 'name played won')
+  const user = await User.findOne({ email }, 'name played won')
+  if(user)
+  {
+    return user;
+  } else {
+    throw new DatabaseError('Could not find email in database.')
+  }
 }
 
-export async function findUserByUsername(username: string) {
-  return User.findOne({ username }, 'name played won')
+export async function findUserByUsername(name: string) {
+  const user = await User.findOne({ name }, 'name played won')
+  if(user) {
+    return user;
+  } else {
+    throw new DatabaseError('Could not find username in database.')
+  }
 }
 
 export async function findEmailAndPass(email: string, password: string) {
-  return User.findOne({ email: email, password: password })
+  const user = await User.findOne({ email: email, password: password })
+  if(user) {
+    return user;
+  } else {
+    throw new DatabaseError('Could not find user in database.')
+  }
 }
 
 //exists() returns null or ObjectId of first item that matches criteria
@@ -36,24 +52,24 @@ export async function userExists(email: string) {
 //Increase games played
 export async function increasePlayed(email: string) {
   const user = await findUserByEmail(email)
-  if (user && user.played) {
+  if (typeof user.played==="number") {
     user.played = user.played + 1
     await user.save()
     return user
   } else {
-    throw new DatabaseError('Could not find email in database.')
+    throw new DatabaseError('something went wrong with user.played')
   }
 }
 
 //Increase wins
 export async function increaseWon(email: string) {
   const user = await findUserByEmail(email)
-  if (user && user.won) {
+  if (typeof user.won==="number") {
     user.won = user.won + 1
     await user.save()
     return user
   } else {
-    throw new DatabaseError('Could not find email in database.')
+    throw new DatabaseError('something went wrong with user.won.')
   }
 }
 
