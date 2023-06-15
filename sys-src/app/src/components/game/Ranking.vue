a46a2a029c2463be1aae7cdf776b39615bfa0b6d
Kevin Paulus <k.paulus@oth-aw.de>
2023-06-15 19:48:08 +0200

    Update Ranking.vue
    Former-commit-id: 266e2ce6bedd011de044bf7fb842febbbd214595

--- sys-src/app/src/components/game/Ranking.vue
+++ sys-src/app/src/components/game/Ranking.vue
@@ -9,16 +9,28 @@ const props = defineProps({
     <div v-if="show">
       <div class="ranking-container">
         <div class="ranking-header">
-          <slot name="header">default header</slot>
+          <slot name="header">Ranking</slot>
         </div>
 
         <div class="ranking-body">
-          <slot name="body">default body</slot>
+          <div>
+            <div>Rank</div>
+            <div>Username</div>
+            <div>Wins</div>
+            <div>Games</div>
+            <div>Winrate</div>
+          </div>
+          <div>
+            <div>#1</div>
+            <div>FastDiscard</div>
+            <div>2</div>
+            <div>3</div>
+            <div>66.66%</div>
+          </div>
         </div>
 
         <div class="ranking-footer">
           <slot name="footer">
-            default footer
             <button class="ranking-default-button" @click="$emit('close')">OK</button>
           </slot>
         </div>
