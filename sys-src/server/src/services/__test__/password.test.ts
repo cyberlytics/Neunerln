4e6c554c44e5d6527a26424305b7ea5b6a7e66da
Berkay Yurdagül <b.yurdaguel@oth-aw.de>
2023-06-25 08:49:17 +0200

    added more tests
    Former-commit-id: bbfaa3049bda4ca7c9050d40813bd60db9de9f53

--- sys-src/server/src/services/__test__/password.test.ts
+++ sys-src/server/src/services/__test__/password.test.ts
@@ -1,16 +1,77 @@
-import { Password } from '../password'
+import { JWT } from '../jwt'
+import { BadRequestError } from '../../errors/bad-request-error'
+import { InternalServerError } from '../../errors/interal-server-error'
 
-describe('password service', () => {
-  it('returns a bcrypt salted hash', async () => {
-    const hash = await Password.toHash('supersecretpassword')
-
-    return expect(hash).toMatch(/\$2b\$\d\d\$.*/)
+describe('JWT Service', () => {
+  beforeAll(() => {
+    process.env.JWT_KEY = 'something'
   })
 
-  it('returns a false on wrong supplied password', async () => {
-    const hash = await Password.toHash('supersecretpassword')
-    const result = await Password.compare('tes', hash)
+  it('returns a token', async () => {
+    const token = await JWT.createToken({ test: 'test' })
+    return expect(token).toBeDefined()
+  })
 
-    return expect(result).toBeFalsy()
+  it('returns error jwt is malformed', async () => {
+    const token = await JWT.createToken({ test: 'test' })
+    const malformed = token?.slice(0, token.length - 2) + 'a'
+
+    expect(token).not.toEqual(malformed)
+    return expect(JWT.verifyToken(malformed)).rejects.toThrow(BadRequestError)
+  })
+
+  it('returns error jwt is expired', async () => {
+    const token = await JWT.createToken({ test: 'test' }, '1ms')
+    await new Promise((r) => setTimeout(r, 50))
+    if (!token) {
+      throw new Error('Something went wrong with the token!')
+    }
+
+    return expect(JWT.verifyToken(token)).rejects.toThrow(BadRequestError)
+  })
+
+  it('returns same decoded object', async () => {
+    const payload = { test: 'test' }
+    const token = await JWT.createToken(payload)
+    if (token) {
+      const obj = await JWT.verifyToken(token)
+      return expect(obj).toMatchObject(payload)
+    } else {
+      return
+    }
+  })
+
+  it('returns error jwt is malformed', async () => {
+    const token = await JWT.createToken({ test: 'test' })
+    const malformed = token?.slice(0, token.length - 2) + 'a'
+
+    expect(token).not.toEqual(malformed)
+    return expect(JWT.verifyToken(malformed)).rejects.toThrow(BadRequestError)
+  })
+
+  it('returns error jwt is expired', async () => {
+    const token = await JWT.createToken({ test: 'test' }, '1ms')
+    await new Promise((r) => setTimeout(r, 50))
+    if (!token) {
+      throw new Error('Something went wrong with the token!')
+    }
+
+    return expect(JWT.verifyToken(token)).rejects.toThrow(BadRequestError)
+  })
+
+  it('returns same decoded object', async () => {
+    const payload = { test: 'test' }
+    const token = await JWT.createToken(payload)
+    if (token) {
+      const obj = await JWT.verifyToken(token)
+      return expect(obj).toMatchObject(payload)
+    } else {
+      return
+    }
+  })
+
+  it('rejects Token because process.env.JWT_KEY is missing', async () => {
+    delete process.env.JWT_KEY
+    expect(JWT.createToken({})).rejects.toThrow(InternalServerError)
   })
 })
